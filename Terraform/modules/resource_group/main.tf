389f20e6e960d8eca6d71a9aad4986898b6906bb
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-26 18:12:09 +0530

    Create main.tf

---
+++ Terraform/modules/resource_group/main.tf
@@ -0,0 +1,9 @@
+locals {
+  full_name = trimsuffix(join("-", [var.product, var.environment, var.location_abbreviation]), "-")
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${local.full_name}"
+  location = var.location
+  tags     = merge(var.tags, { service = "rg" })
+}
