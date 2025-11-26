44d679e314e7b1bce5b38180049644ed793b91f9
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-26 18:17:12 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,29 +1,3 @@
-output "resource_group_name" {
-  description = "Resource group name"
-  value       = azurerm_resource_group.main.name
-}
-
-output "aks_cluster_name" {
-  description = "AKS cluster name"
-  value       = module.aks.aks_name
-}
-
-output "acr_name" {
-  description = "ACR name"
-  value       = module.acr.acr_name
-}
-
-output "key_vault_name" {
-  description = "Key Vault name"
-  value       = module.key_vault.key_vault_name
-}
-
-output "app_service_url" {
-  description = "App Service URL"
-  value       = "https://${module.appservice.default_hostname}"
-}
-
-output "vnet_name" {
-  description = "Virtual Network name"
-  value       = module.network.vnet_name
+output "kubelet_object_id" {
+  value = module.aks.kubelet_managed_identity_object_id
 }
