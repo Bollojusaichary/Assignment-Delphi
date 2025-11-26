f38844f66e6b37e8d3fb907eac22ed4378f95c88
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-26 18:14:31 +0530

    Update outputs.tf

--- Terraform/modules/vnet/outputs.tf
+++ Terraform/modules/vnet/outputs.tf
@@ -1,34 +1,7 @@
-output "vnet_id" {
-  description = "Virtual Network ID"
-  value       = azurerm_virtual_network.main.id
+output "name" {
+  value = azurerm_virtual_network.vnet.name
 }
 
-output "vnet_name" {
-  description = "Virtual Network name"
-  value       = azurerm_virtual_network.main.name
-}
-
-output "aks_subnet_id" {
-  description = "AKS subnet ID"
-  value       = azurerm_subnet.aks.id
-}
-
-output "app_service_subnet_id" {
-  description = "App Service subnet ID"
-  value       = azurerm_subnet.app_service.id
-}
-
-output "key_vault_subnet_id" {
-  description = "Key Vault subnet ID"
-  value       = azurerm_subnet.key_vault.id
-}
-
-output "private_endpoints_subnet_id" {
-  description = "Private endpoints subnet ID"
-  value       = azurerm_subnet.private_endpoints.id
-}
-
-output "network_security_group_id" {
-  description = "Network Security Group ID"
-  value       = azurerm_network_security_group.aks.id
+output "id" {
+  value = azurerm_virtual_network.vnet.id
 }
