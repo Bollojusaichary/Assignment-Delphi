397a8a956d68880059d92356bad67aac8a17fba2
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-26 18:15:08 +0530

    Create main.tf

---
+++ Terraform/modules/subnet/main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_subnet" "subnet" {
+  name                                           = var.subnet_name
+  resource_group_name                            = azurerm.resource_group_name.rg.name
+  virtual_network_name                           = var.virtual_network_name
+  address_prefixes                               = var.address_prefix
+  service_endpoints                              = var.service_endpoints
+  # enforce_private_link_endpoint_network_policies = var.enforce_private_link_endpoint
+  # enforce_private_link_service_network_policies  = var.enforce_private_link_service
+
+
+  dynamic "delegation" {
+    for_each = var.subnet_delegation
+    content {
+      name = delegation.key
+      dynamic "service_delegation" {
+        for_each = toset(delegation.value)
+        content {
+          name    = service_delegation.value.name
+          actions = service_delegation.value.actions
+        }
+      }
+    }
+  }
+}
