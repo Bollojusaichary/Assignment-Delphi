b3f9f80d673082f02dd9589d6f3a63dacd7a7e59
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-26 18:17:39 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,82 +1,71 @@
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_id" {
-  description = "Azure service principal client ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_secret" {
-  description = "Azure service principal client secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "resource_group_name" {
-  description = "Resource_group"
-  type        = string
-}
-
-variable "project_name" {
-  description = "Project name for resource naming"
-  type        = string
-  default     = "prodapp"
-}
-
-variable "environment" {
-  description = "Environment name (dev, staging, production)"
-  type        = string
-  default     = "production"
-}
-
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "uaenorth"
+variable "product" {
+  description = "The name of the product this service bus is for."
+  default     = "delphi"
 }
 
 variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default = {
-    Environment = "production"
-    Project     = "demo-app"
-    ManagedBy   = "terraform"
-  }
+  description = "Tags to apply to this resource."
+  default        = "prod"
 }
 
-# AKS Variables
-variable "aks_version" {
-  description = "Kubernetes version"
+variable "subnet_name" {
   type        = string
-  default     = "1.27.3"
+  description = "Specifies the name for the network subnet"
+  default     = "xxxxxx"
 }
 
-variable "aks_node_count" {
-  description = "Number of AKS nodes"
-  type        = number 
-  default     = 3
-}
-
-variable "aks_vm_size" {
-  description = "AKS node VM size"
+variable "subnet_name" {
   type        = string
-  default     = "Standard_B4ms"
+  description = "Specifies the name for the network subnet"
+  default     = "xxxxxxx"
 }
 
-# Network Variables
+variable "location" {
+  description = "The location/region where the service bus is created."
+  default     = "uaenorth"
+}
+
 variable "vnet_address_space" {
-  description = "VNet address space"
+  description = "The network address space of this VNet."
   type        = list(string)
-  default     = ["10.0.0.0/16"]
+  default     = "xxxxx" 
+}
+
+variable "location_abbreviation" {
+  description = "The short location name, according to DevOps standards."
+  default     = "uaenorth"
+}
+
+variable "environment" {
+  description = "The environment this service bus is situated within."
+  type        = string
+}
+variable "subscription_id" {
+  description = "Azure Subscription ID for PhlexVision"
+  type        = string
+  default     = "f0d3757c-9b40-43d3-ba6b-e3be083b0dd3"
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID for PhlexVision"
+  type        = string
+  default     = "xxxxxxx"
+}
+
+variable "client_id" {
+  description = "Client ID/App ID for associated subscription (Terraform SP)"
+  type        = string
+  default     = "xxxxxxx"
+}
+
+variable "client_object_id" {
+  description = "Object ID of the above app (Terraform SP)"
+  type        = string
+  default     = "xxxxxxxx"
+}
+
+variable "client_secret" {
+  description = "Secret for the above app (Terraform SP)"
+  type        = string
+  default     = "xxxxxxxxx"
 }
