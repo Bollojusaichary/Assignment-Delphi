66a8b9e52b49eed73c1ff4c8415110d9b9fe8bff
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-27 15:24:24 +0530

    Update outputs.tf

--- Terraform/modules/aks/outputs.tf
+++ Terraform/modules/aks/outputs.tf
@@ -3,7 +3,7 @@ value = azurerm_kubernetes_cluster.this.name
 }
 
 output "kubelet_identity_object_id" { 
-value = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id 
+value = azurerm_kubernetes_cluster.this.kubelet_identity.object_id 
 }
 
 output "kubeconfig_raw" { 
