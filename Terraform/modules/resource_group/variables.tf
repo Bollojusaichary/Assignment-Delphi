21453df640f9d196674969451588fc56943c4c3a
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-27 15:28:56 +0530

    Update variables.tf

--- Terraform/modules/resource_group/variables.tf
+++ Terraform/modules/resource_group/variables.tf
@@ -1,16 +1,5 @@
-# ---------------- METADATA ------------------------
 variable "location" {
   description = "The location/region where the resource group is created."
-  type        = string
-}
-
-variable "location_abbreviation" {
-  description = "The short location name, according to DevOps standards."
-  type        = string
-}
-
-variable "product" {
-  description = "The name of the product this resource group is for."
   type        = string
 }
 
@@ -23,4 +12,3 @@ variable "tags" {
   description = "Tags to apply to this resource."
   type        = map(string)
 }
-# ------------- END METADATA -----------------------
