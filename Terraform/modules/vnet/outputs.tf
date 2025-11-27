8b89c5508584b5b35d9e25d0a4bf6fb62fc0719f
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-27 15:30:55 +0530

    Update outputs.tf

--- Terraform/modules/vnet/outputs.tf
+++ Terraform/modules/vnet/outputs.tf
@@ -1,7 +1,19 @@
-output "name" {
-  value = azurerm_virtual_network.vnet.name
+variable "location" {
+  description = "The location/region where the VNet is created."
+  type        = string
 }
 
-output "id" {
-  value = azurerm_virtual_network.vnet.id
+variable "environment" {
+  description = "The environment this VNet resides within."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to this resource."
+  type        = map(string)
+}
+
+variable "vnet_cidr_address_space" {
+  description = "The network address space of this VNet."
+  type        = string
 }
