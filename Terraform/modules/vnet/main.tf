c95976099c63732f9afd4079eee6017e8dc9889f
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-28 22:42:58 +0530

    Update main.tf

--- Terraform/modules/vnet/main.tf
+++ Terraform/modules/vnet/main.tf
@@ -1,11 +1,11 @@
 locals {
-  full_name = trimsuffix(join(var.environment,"vnet"))
+  fullname = "${var.environment}-${var.location}" 
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = local.full_name
-  address_space       = var.vnet_cidr_address_space
-  location            = var.location
+  name                = "vnet"-"${local.fullname}"
   resource_group_name = module.resource_group.name
-  tags                = merge(var.tags, { service = "vnet" })
+  location            = var.location
+  address_space       = var.vnet_address_space
+  tags                = merge(var.environment, {service = "vnet"})    
 }
