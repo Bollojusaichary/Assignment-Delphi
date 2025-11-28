f9e83e45a1e05b8c3dce4b338864d03738b39d69
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-28 22:45:01 +0530

    Update variables.tf

--- Terraform/modules/akv/variables.tf
+++ Terraform/modules/akv/variables.tf
@@ -1,51 +1,33 @@
-# ---------------- METADATA ------------------------
 variable "location" {
-  description = "The location/region where the VNet is created."
-  type        = string
+  type = string
+  description = "this is prod location."
 }
-
-
-variable "environment" {
-  description = "The environment this VNet resides within."
-  type        = string
-}
-
-variable "name_suffix" {
-  description = "Optional suffix to append to the VNet name."
-  type        = string
-  default     = ""
-}
-
-variable "vault_resource_name" {
-  description = "String used to identify the type of resource being created"
-  type        = string
-  default     = "vault"
-}
-
-variable "tags" {
-  description = "Tags to apply to this resource."
-  type        = map(string)
-}
-# ------------- END METADATA -----------------------
-#------------------------ AZURE AD OBJECT ID -------------------------#
 
 variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
+  type = string
+  description = "this is prod tenant id."
+}
+
+variable "environment" {
+  type = string
+  description = "this is prod env."
+}
+
+variable "purge_protection_enabled" {
+  type = string
+  description = "this is prod env."
 }
 
 variable "client_object_id" {
-  description = "Object ID of the above app (Terraform SP)"
-  type        = string
+  type = string
+  description = "this is prod env."
 }
 
 variable "access_policy_object_ids" {
-  description = "The object IDs that are allowed to access this key vault"
-  type        = list(string)
+  type = list(string)
+  description = "this is prod env."
 }
 
-#------------------------ END AZURE AD OBJECT ID -------------------------#
-#------------------------ SECRETS --------------------------#
 variable "secrets" {
   description = "The secrets to add to the vault."
   type = list(object({
@@ -54,52 +36,9 @@ variable "secrets" {
   }))
   default = []
 }
-#------------------------ END SECRETS --------------------------#
-
-# ---------------------- DEPENDENCIES -----------------------#
-variable "resource_group_name" {
-  description = "The name of the resource group to create this VNet in."
-  type        = string
-}
-# -------------------- END DEPENDENCIES --------------------#
-
-#----------------PRIVATE ENDPOINT--------------------#
-variable "create_private_endpoint" {
-  type        = bool
-  description = "Create private endpoint for key vault"
-  default     = false
-}
 
 variable "pe_subnet_id" {
   type        = string
-  description = "Subnet to attach Private Endpoint"
+  description = "Subnet to attach Private Endpoint to"
   default     = null
 }
-
-variable "private_dns_zone" {
-  type        = string
-  description = "Resource ID of the private dns zone to create the private endpoint dns records in"
-  default     = "/subscriptions/xxxxxxxxxxxxxxxxx/resourceGroups/rg-xxxxx-vpn-uaenorth/providers/Microsoft.Network/privateDnsZones/privatelink.vaultcore.azure.net"
-}
-
-#----------------END PRIVATE ENDPOINT--------------------#
-#-------------VAULT PROTECTION SETTINGS-------------------#
-
-variable "network_acls" {
-  description = "Key Vault Network ACL settings"
-  type        = any
-  default     = null
-}
-
-variable "purge_protection_enabled" {
-  description = "To enable/disable KeyVault Purge protection"
-  type        = bool
-  default     = false
-}
-
-variable "enable_resource_lock" {
-  description = "To enable/disable KeyVault Resource Lock"
-  type        = bool
-  default     = false
-}
-#-------------END VAULT FW SETTINGS-------------------#
