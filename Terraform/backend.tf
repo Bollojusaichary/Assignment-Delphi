d30ef8811de2bda7e0dcdce7104e6c45fa97f0f4
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-28 22:48:11 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name   = "rgxxx"
-        storage_account_name  = "stgacnttf"
-        container_name        = "tfstate-xxxx"
-        key                   = "xxxx.terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name    = "xxxxxxx"
+    storage_account_name   = "xxxxxx"
+    container_name         = "xxxxxxx"
+    key                    = "xxxxxxx"
+  }
 }
