915c7a9a4b4aefac3f25601e3fcef446ae3d9678
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-28 22:40:16 +0530

    Update variables.tf

--- Terraform/modules/appservice/variables.tf
+++ Terraform/modules/appservice/variables.tf
@@ -1,37 +1,13 @@
-variable "tags" {
-  description = "Tags to apply to this resource."
-  type        = map(string)
+variable "environment" {
+  type = string
+  description = "this is prod env."
 }
 
 variable "location" {
-  description = "The location/region where the resource group is created."
-  type        = string
+  type = string
+  description = "this is prod location."
 }
 
-variable "name_suffix" {
-  description = "Optional suffix to append to the service bus name."
-  type        = string
-  default     = ""
-}
-
-variable "environment" {
-  description = "The environment this service bus is situated within."
-  type        = string
-}
-
-# ------------- DEPENDENCIES -----------------------
-
-variable "subnet_id" {
-  type        = string
-  description = "WebApp subnet ID used for connecting azure app service to SQL database"
-}
-
-variable "storage_conn_string" {
-  type        = string
-  description = "Connection string of PhlexVision Storage Account"
-}
-# ------------- END DEPENDENCIES -------------------#
-#------------------CONFIGURATION--------------------#
 variable "asp_tier" {
   type        = string
   description = "App Service Plan tier."
@@ -43,29 +19,19 @@ variable "asp_size" {
   description = "App Service Plan Size."
   default     = "S1"
 }
-#------------------END CONFIGURATION--------------------#
-#-------------------SECRETS-------------------#
-variable "mock_api_token" {
-  type        = map(string)
-  description = "Mock Api Token Map for various environments"
-  default = {
-    dev = "#{vision-dev-jwt-secret-mock-app}#"
-    int = "#{vision-int-jwt-secret-mock-app}#"
-    stg = "#{vision-stg-jwt-secret-mock-app}#"
-  }
-}
-variable "identity_ids" {
-  description = "The name to give the user assigned managed identity"
-  type        = list(string)
-  default     = null
-}
-variable "identity_type" {
-  description = "The type of user assigned managed identity"
+
+variable "storage_conn_string" {
   type        = string
+  description = "App Service Plan Size."
+}
+
+variable "identity_type" {
+  type        = string
+  description = "App Service Plan Size."
   default     = null
 }
 
-variable "password" {
-  description = "Secret for the app setting"
-  default     = "#{phlex-mock-secret}#"
+variable "subnet_id" {
+  type        = string
+  description = "WebApp subnet ID used for connecting azure app service to SQL database"
 }
