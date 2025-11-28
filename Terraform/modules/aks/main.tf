ce56ef5059b44e843b4da4832b9218a14161c880
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-28 22:38:21 +0530

    Update main.tf

--- Terraform/modules/aks/main.tf
+++ Terraform/modules/aks/main.tf
@@ -1,33 +1,15 @@
-resource "azurerm_virtual_network" "aksvnet" {
-  name                = "vnet-aks-${var.environment}"
-  location            = var.location
-  resource_group_name = module.resource_group.name
-  address_space       = ["10.0.0.0/16"]
+locals {
+  fullname = "${var.environment}-${var.location}" 
 }
 
-resource "azurerm_subnet" "akssubnet" {
-  name                 = "snet-aks"
-  resource_group_name  = module.resource_group.name
-  virtual_network_name = azurerm_virtual_network.aksvnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_log_analytics_workspace" "this" {
-  name                = "log-${var.environment}"
-  location            = var.location
-  resource_group_name = module.resource_group.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
-resource "azurerm_kubernetes_cluster" "this" {
+resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks--${var.environment}"
   location            = var.location
   resource_group_name = module.resource_group.name
   dns_prefix          = lower("aks--${var.environment}")
 
   default_node_pool {
-    name           = "prodpool"
+    name           = "aksnode-${local.fullname}"
     vm_size        = "Standard_D8s_v5"
     zones          = ["1", "2", "3"]
     # enable_auto_scaling = true
@@ -47,11 +29,6 @@ resource "azurerm_kubernetes_cluster" "this" {
     load_balancer_sku = "standard"
   }
 
-  oms_agent {
-    # enabled                    = true
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
-  }
-
   azure_policy_enabled = true
   local_account_disabled = true
   role_based_access_control_enabled = true
@@ -62,7 +39,7 @@ resource "azurerm_kubernetes_cluster" "this" {
 }
 
 resource "azurerm_role_assignment" "acr_pull" {
-  scope                = var.acr_id
+  scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.this.kubelet_identity.object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.object_id
 }
