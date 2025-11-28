a62d818c01919cab47c0a01bd12ecf98157dd82a
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-28 22:49:06 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,52 +1,61 @@
-module "resource_group" {
-  source = "./modules/resource_group"
+module "resourcegroup" {
+  source = "./modules/resourcegroup"
 
   location              = var.location
   environment           = var.environment
-  tags                  = local.tags
 }
-
 
 module "keyvault" {
   source = "./modules/keyvault"
 
-  vault_resource_name       = module.keyvault.name
-  location                  = var.location
-  resource_group_name       = module.resource_group.name
-  environment               = var.environment
-  tags                      = merge(local.tags, { service = "kv" })
-  access_policy_object_ids  = azurerm.key_vault.keyvault.id
-  client_object_id          = var.client_object_id
-  tenant_id                 = var.tenant_id
+  vault_resource_name   = module.keyvault.name 
+  location              = var.location
+  environment           = var.environment
+  tags                  = merge({ service = "kv"}, var.environment)
+
+  tenant_id        = var.tenant_id
+  client_object_id = var.client_object_id
+
+  purge_protection_enabled = var.purge_protection_enabled
+
+  secrets = var.create_key_vault_private_endpoint ? concat([
+    {
+      name  = "NewRelicLicenseKey"
+      value = var.new_relic_license_key
+    },
+    {
+      name  = "SubscriptionKey"
+      value = var.subscription_id
+    },
+  ], local.keyvault_secrets) : []
+
+  resource_group_name      = module.resource_group.name
+  access_policy_object_ids = var.create_key_vault_private_endpoint ? local.access_policy_object_ids : []
+
+  create_private_endpoint = var.create_key_vault_private_endpoint
+  pe_subnet_id            = var.create_key_vault_private_endpoint ? module.network.subnet_id : null
+
+  network_acls = var.create_key_vault_private_endpoint ? {
+    kv_nacls_bypass_rule    = "None" #Allowed values = 'AzureServices' and 'None'
+    kv_nacls_default_action = "Deny"
+    kv_nacls_subnet_ids     = [module.network.subnet_id_webapp, azurerm_subnet.cluster_node_pool_subnet.id]
+  } : null
+
+  depends_on = [module.SQL, module.app_service, module.aks]
 }
 
-module "vnet" {
-  source = "./modules/vnet"
+
+module "network" {
+  source = "./modules/network"
   providers = {
     azurerm.bastion = azurerm.bastion
   }
-  tags                    = merge(local.tags, { service = "vnet" })
-  environment             = var.environment
-  location                = var.location
-  vnet_cidr_address_space = var.vnet_cidr_address_space
-}
-
-
-module "acr" {
-  source = "./modules/ACR"
-
-  location                  = var.location
-  environment               = var.environment
-  aks_kubelet_identity_id   = "ssdf"
-}
-
-# create AKS node pool subnet
-resource "azurerm_subnet" "cluster_node_pool_subnet" {
-  name                 = var.sub_net.names
-  resource_group_name  = module.resource_group.name
-  virtual_network_name = module.network.vnet
-  address_prefixes     = [var.cluster_node_subnet]
-  service_endpoints    = var.subnet_service_endpoints
+  tags                  = merge({ service = "vnet" }, var.environment)
+  environment           = var.environment
+  location              = var.location
+  resource_group_name   = module.resourcegroup.name
+  vnet_address_space    = [var.vnet_cidr_address_space]
+  subnet_names          = local.subnet_cidr
 }
 
 resource "azurerm_subnet_network_security_group_association" "cluster_node_pool_subnet_nsg" {
@@ -54,64 +63,51 @@ resource "azurerm_subnet_network_security_group_association" "cluster_node_pool_
   network_security_group_id = module.nsg.id
 }
 
-module "aks" {
-  source = "./modules.phlexglobal.com/devops/akscluster/azure"
-  providers = {
-    azurerm.acr = azurerm.acr
-  }
-
-  version                 = "0.5.3"
-  location                = var.location
-  environment             = var.environment
-  tags                    = merge(local.tags, { service = "aks" })
-  resource_group_name     = module.resource_group.name
-  availability_zones      = [1, 2, 3]
-  node_pool_subnet_id     = azurerm_subnet.cluster_node_pool_subnet.id
-  node_vm_class           = var.vmsize
-  azure_policy_enabled    = var.azure_policy_enabled
+resource "azurerm_subnet" "cluster_node_pool_subnet" {
+  name                 = var.subnet_names
+  resource_group_name  = module.resource_group.name
+  virtual_network_name = module.network.vnet
+  address_prefixes     = [var.cluster_node_subnet]
+  service_endpoints    = var.subnet_service_endpoints
 }
 
-module "aks_resources" {
-  source = "modules.phlexglobal.com/devops/aksresources/azure"
+module "AKS" {
+  source = "./modules/AKS"
 
-  version                        = "1.0.10"
-  location                       = var.location
-  resource_group_name            = module.resource_group.name
-  node_resource_group            = module.aks.node_resource_group
-  aks_name                       = module.aks.name
-  cluster_name                   = module.aks.name
-  nginx_ingress_chart_version    = "4.8.2"
-  new_relic_bundle_chart_version = "5.0.41" #To support lowDataMode
-  install_nginx_ingress          = var.install_nginx_ingress
-  install_cert_manager           = var.install_cert_manager
-  install_new_relic_bundle       = var.install_new_relic_bundle
-  nginx_ingress_replica_count    = var.nginx_ingress_replica_count
+  tags                = merge({ service = "AKS" }, var.environment)
+  environment         = var.environment
+  location            = var.location
+  resource_group_name = module.resourcegroup.name
+
+  aks_name            = module.AKS.name
+  kubernetes_version  = var.kubernetes_version
+
+  node_pool_name      = var.node_pool_name
+  vnet_subnet_id      = module.network.subnet_ids["aks"]
 }
 
-module "app_service" {
-  source = "./modules/app_service"
- 
-  location              = var.location
-  environment           = var.environment
-  tags                  = merge(local.tags, { service = "webapp" })
+module "ACR" {
+  source = "./modules/ACR"
 
-  asp_tier            = var.asp_tier
-  asp_size            = var.asp_size
-  subnet_id           = module.network.subnet_id_webapp
-  storage_conn_string = module.storage_account.connection_string
-  identity_type       = ["SystemAssigned", "UserAssigned"]
-  identity_ids        = ["${data.azurerm_user_assigned_identity.identity.id}"]
+  tags                = merge({ service = "acr"}, var.environment)
+  environment         = var.environment
+  location            = var.location
+  resource_group_name = module.resourcegroup.name
+
+  acr_name            = module.ACR.name
+  sku                 = "Standard"
+  admin_enabled       = true
+
+  # ----------------------------
+  # Network Rules (Optional)
+  # ----------------------------
+  public_network_access_enabled = var.acr_public_network_access_enabled   # false = private only
+  allowed_ip_ranges             = var.acr_allowed_ip_ranges               # ["X.X.X.X/32", ...]
+
+  # ----------------------------
+  # Private Endpoint (Optional)
+  # ----------------------------
+  private_endpoint_enabled      = var.acr_private_endpoint_enabled
+  subnet_id                     = module.network.subnet_ids["acr-pe"]     # your PE subnet
 }
 
-module "subnet" {
-  source = "./modules/subnet"
-  providers = {
-    azurerm.bastion = azurerm.bastion
-  }
-  tags                    = merge(local.tags, { service = "subnet" })
-  environment             = var.environment
-  location                = var.location
-  vnet_name               = module.vnet.name
-  vnet_cidr_address_space = var.vnet_cidr_address_space
-  service_endpoints       = ["Microsoft.Web"]
-}
