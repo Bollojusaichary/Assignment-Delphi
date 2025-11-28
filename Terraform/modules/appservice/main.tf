fb0cbbe8a88d0478e902eb92a6609ad986c134ba
Sai Bolloju <bollojusaichary25@gmail.com>
2025-11-28 22:39:34 +0530

    Update main.tf

--- Terraform/modules/appservice/main.tf
+++ Terraform/modules/appservice/main.tf
@@ -1,56 +1,48 @@
 locals {
-  full_name = trimsuffix(join([var.environment]))
+  fullname = "${var.environment}-${var.location}" 
+
+    env = tomap({
+    prod = "Production"
+  })
 }
 
-resource "azurerm_app_service_plan" "example" {
-  count               = var.environment != "prod" ? 1 : 0
-  name                = "asp-${local.full_name}"
+resource "azurerm_app_service_plan" "asp" {
+  name                = "asp-${local.fullname}"
   location            = var.location
-  resource_group_name = module.resource_group_name
+  resource_group_name = module.resource_group.name
   kind                = "Linux"
-  reserved            = true
-  tags                = merge(var.tags, { service = "asp" })
+  tags                = merge({ service = "asp" }, var.environment)
   sku {
     tier = var.asp_tier
     size = var.asp_size
   }
 }
 
-resource "azurerm_app_service" "app_service" {
-  count               = var.environment != "prod" ? 1 : 0
-  name                = "as-${local.full_name}"
+resource "azurerm_linux_web_app" "appservice" {
+  name                = "appsvc-${local.fullname}"
+  resource_group_name = module.resource_group.name
   location            = var.location
-  resource_group_name = azurerm.resource_group_name.rg.name
-  app_service_plan_id = azurerm_app_service_plan.example[0].id
-  tags                = merge(var.tags, { service = "as" })
+  service_plan_id     = azurerm_app_service_plan.asp.id
+
   site_config {
-    linux_fx_version          = "DOTNETCORE|6.0"
-    use_32_bit_worker_process = true
-    ftps_state                = "FtpsOnly"
-    always_on                 = true
+    always_on         = true
+    ftps_state        = "FtpsOnly"
+    use_32_bit_worker = false
   }
 
   app_settings = {
-    "ASPNETCORE_ENVIRONMENT"          = "${lookup(var.environment)}"
-    "BlobConnectionString"            = "${var.storage_conn_string}"
-    "keyVaultName"                    = "xxxxxx-${var.environment}"
-    "TokenKey"                        = "${lookup(var.mock_api_token, var.environment)}"
-    "Password"                        = var.password
-    "UserId"                          = "mock"
-    "WEBSITE_ENABLE_SYNC_UPDATE_SITE" = "true"
-    "WEBSITE_RUN_FROM_PACKAGE"        = "1"
+    "BlobConnectionString"     = "${var.storage_conn_string}"
+    "keyVaultName"             = module.AKS.name
+    "ASPNETCORE_ENVIRONMENT"   = "${lookup(local.env, var.environment)}"
   }
 
   https_only          = true
-  client_cert_enabled = false
   identity {
     type         = var.identity_type
-    identity_ids = var.identity_ids
   }
 }
 
-resource "azurerm_app_service_virtual_network_swift_connection" "webapp_connection" {
-  count          = var.environment != "prod" ? 1 : 0
-  app_service_id = azurerm_app_service.app_service[0].id
+resource "azurerm_app_service_virtual_network_swift_connection" "appsvc_connection" {
+  app_service_id = azurerm_linux_web_app.appservice.id
   subnet_id      = var.subnet_id
 }
